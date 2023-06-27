b50959b33ca8dd68eefb8456b6fd5fc5fa9b803f
RduMarais <rmichon@telecom-paristech.fr>
2023-06-27 11:36:11 +0200

    test with asyncio 2

--- pollsite/poll/twitch_handler.py
+++ pollsite/poll/twitch_handler.py
@@ -283,9 +283,9 @@ class NewTwitchHandler(threading.Thread):
 	def terminate(self):
 		if(hasattr(self,'event_sub')):
 			loop = asyncio.get_event_loop()
-			loop.run_until_complete(asyncio.wait(ayncio.async(self.stop_event_sub())))
+			loop.run_until_complete(asyncio.wait(asyncio.async(self.stop_event_sub())))
 			loop.close()
-			asyncio.run(self.stop_event_sub())
+			# asyncio.run(self.stop_event_sub())
 		if(hasattr(self,'chat')):
 			self.chat.stop()
 		if(settings.DEBUG) : print('NTW : stopped')
